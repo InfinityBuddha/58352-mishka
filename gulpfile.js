a61344ed8b7aaf583fb5e14d1a8e26d0770f4274
Руслан Королев <techfusionwork@gmail.com>
2016-12-27 00:30:50 +0300

    Фикс catalog.html на IE

--- gulpfile.js
+++ gulpfile.js
@@ -27,10 +27,10 @@ gulp.task("style", function() {
         sort: true
       })
     ]))
-    .pipe(gulp.dest("css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(minify())
-    //.pipe(rename("style.min.css"))
-    .pipe(gulp.dest("css"))
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(server.stream());
 });
 
